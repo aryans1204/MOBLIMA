bb8abaccbf562afa6c4411d6bc664bf2eb2baf17
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2022-10-24 19:27:22 +0800

    Staff update

--- CinemaAbs.java
+++ CinemaAbs.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public abstract class CinemaAbs {
     Movie[] movies; //list of movies available at the cinema;
-    HashMap<String, HashMap<Date, HashMap<Integer, Customer>>> seats;  //returns customer if taken else null for a movie for a showtime at cinema
-    HashMap<String, HashMap<String, HashMap<Date, String>>> tID;  //format is map.get(username).get(movie.title).get(movie.showtime) return tID
-    HashMap<String, HashMap<Date, Integer>> free;  //free seats
+    HashMap<String, HashMap<String, HashMap<Integer, Customer>>> seats;  //returns customer if taken else null for a movie for a showtime at cinema
+    HashMap<String, HashMap<String, HashMap<String, String>>> tID;  //format is map.get(username).get(movie.title).get(movie.showtime) return tID
+    HashMap<String, HashMap<String, Integer>> free;  //free seats
+    int code;
 
     public abstract void printLayout(Movie movie, Date showtime);   //print seat layout for a given Movie based on showtime.
 
