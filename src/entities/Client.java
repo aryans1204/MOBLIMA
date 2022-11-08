66f336714b22e6cf78a4cedb5393b800133103e0
Aryan <91662750+aryans1204@users.noreply.github.com>
2022-11-08 17:29:21 +0800

    Update Client.java

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,12 +1,14 @@
 package entities;
 
 import java.io.*;
+import java.util.*;
+
 public interface Client {
     public String username = null; //client username
     public String password = null;
 
-    public boolean login(ArrayList<Cinema> cinemaDB) throws IOException;
+    public boolean login(ArrayList<Object> clientDB) throws IOException;
 
-    public boolean createAccount(ArrayList<Cinema> cinemaDB) throws IOException;
+    public boolean createAccount(ArrayList<Object> clientDB) throws IOException;
 
 }
