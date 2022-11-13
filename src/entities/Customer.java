7d2df249fcbeb1a1cb355716b415dc151a2a5954
Darren <darrennjk@gmail.com>
2022-11-13 13:16:03 +0800

    Added Customer JavaDoc

--- src/entities/Customer.java
+++ src/entities/Customer.java
@@ -9,18 +9,57 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Represents a Customer/Movie-goer in the system
+ */
 @SuppressWarnings("serial")
 public class Customer implements Client, Serializable {
+    /**
+     * Customer's email
+     */
     private String email;
+    /**
+     * Customer's mobile number
+     */
     private int mobileNumber;
+    /**
+     * Customer's name
+     */
     private String name;
+    /**
+     * Customer's age
+     */
     private int age;
+    /**
+     * Customer's authentication indicator. Whether customer is authenticated or not. All methods check for authentication.
+     */
     private boolean auth = false;  //authentication indicator. Whether customer is authenticated or not. All methods must check for authentication.
+    /**
+     * Customer's username
+     */
     private String username;
+    /**
+     * Customer's password
+     */
     private String password;
+    /**
+     * Array list of Ticket object to store Customer's bookings
+     */
     private ArrayList<Ticket> bookings = new ArrayList<>();
+    /**
+     * ClientController to get Customer's data from the database
+     */
     private static ClientController cl;
 
+    /**
+     * Creates a Customer/Movie-goer with the given attributes
+     * @param name			This Customer's name
+     * @param age			This Customer's age
+     * @param username		This Customer's username
+     * @param password		This Customer's password
+     * @param email			This Customer's email
+     * @param mobileNumber	This Customer's mobile number
+     */
     //Constructor for Customer object.
     public Customer(String name, int age, String username, String password, String email, int mobileNumber) {
         this.name = name;
@@ -31,7 +70,12 @@ public class Customer implements Client, Serializable {
         this.mobileNumber = mobileNumber;
     }
 
-
+    /**
+     * A method for logging into the system and set auth to True
+     * @param tempUsername  System will look for this username in the database
+     * @return  Returns true for successful login and false for unsuccessful login
+     * @throws IOException
+     */
     public boolean login(String tempUsername) throws IOException {
         int tries = 9;
         String tempPassword;
@@ -59,49 +103,9 @@ public class Customer implements Client, Serializable {
 
     }
 
-    public boolean createAccount(ArrayList<Staff> staffDB, ArrayList<Customer> customerDB) throws IOException {
-        int tries = 9;  //9 tries before system shuts;
-        String tempUsername;
-        String tempPassword;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        do {
-            System.out.println("Enter username: ");
-            tempUsername = reader.readLine();
-            if (cl.checkUsernameExist(tempUsername)) System.out.println("Username already taken");
-            tries--;
-            if (tries == 0) System.out.println("Too many tries. System quitting now");
-        } while (cl.checkUsernameExist(tempUsername) && tries != 0);
-        if (tries == 0) return false;
-        setUsername(tempUsername);
-        System.out.println("Enter password: ");
-        tempPassword = reader.readLine();
-        setPassword(tempPassword);
-        System.out.println("Enter your name: ");
-        String tempName = reader.readLine();
-        setName(tempName);
-        System.out.println("Enter email: ");
-        String tempEmail = reader.readLine();
-        setEmail(tempEmail);
-        System.out.println("Enter mobile number: ");
-        int tempNumber = Integer.parseInt(reader.readLine());
-        setNumber(tempNumber);
-        System.out.println("Enter age: ");
-        int tempage = Integer.parseInt(reader.readLine());
-        setAge(tempage);
-        customerDB.add(this);
-        System.out.println("Account created successfully");
-        return true;
-    }
-
-    public static ClientController getCl() {
-        return cl;
-    }
-
-    public static void setCl(ClientController cl) {
-        Customer.cl = cl;
-    }
-
-
+    /**
+     * A method for Customer's to view their past bookings
+     */
     public void viewBookings() {
         System.out.println("Your past bookings are available here");
         for (Ticket ticket : this.bookings) {
@@ -109,65 +113,124 @@ public class Customer implements Client, Serializable {
         }
     }
 
+    /**
+     * A method to check the authentication status of the Customer
+     * @return
+     */
     public boolean isAuth() {
         return auth;
     }
 
+    /**
+     * Gets the name of this Customer
+     * @return  this Customer's name
+     */
     //Getters
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the age of this Customer
+     * @return  this Customer's age
+     */
     public int getAge() {
         return age;
     }  //method to get the age of the client, as an int.
 
+    /**
+     * Gets the username of this Customer
+     * @return  this Customer's username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets the password of this Customer
+     * @return  this Customer's password
+     */
     public String getPassword() {
         return password;
     }
 
-
+    /**
+     * Gets the mobile number of this Customer
+     * @return  this Customer's mobile number
+     */
     public int getMobileNumber() {
         return this.mobileNumber;
     }
 
+    /**
+     * Gets the email of this Customer
+     * @return  this Customer's email
+     */
     public String getEmail() {
         return this.email;
     }
 
+    /**
+     * Changes the name of this Customer
+     * @param name  This Customer's new name
+     */
     //Setters
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Changes the age of this Customer
+     * @param age   This Customer's new age
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * Changes the username of this Customer
+     * @param username  This Customer's new username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Changes the password of this Customer
+     * @param password  This Customer's new password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Changes the email of this Customer
+     * @param email This Customer's new email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Changes the mobile number of this Customer
+     * @param mobileNumber  This Customer's new mobile number
+     */
     public void setNumber(int mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
 
+    /**
+     * A method to add bookings for Customer
+     * @param t
+     */
     public void addBookings(Ticket t) {
         this.bookings.add(t);
     }
 
+    /**
+     * Overrides toString method to store Customer detail in a specific format
+     * @return  a string of Customer details
+     */
     @Override
     public String toString() {
         String customerDetail;
