bc22a56f7b51937899cb2764a2cc376421997c2b
Darren <darrennjk@gmail.com>
2022-11-13 15:23:07 +0800

    Added MovieStatus and MovieType JavaDoc

--- src/entities/MovieType.java
+++ src/entities/MovieType.java
@@ -1,7 +1,20 @@
 package src.entities;
 
+/**
+ * Enum of Movie types
+ * @author JinCheng
+ */
 public enum MovieType {
+    /**
+     * 2-D Movies
+     */
 	TWO_D,
+    /**
+     * 3-D Movies
+     */
     THREE_D,
+    /**
+     * Blockbuster Movies
+     */
     BLOCKBUSTER;
 }
